0f67ee82bb77108be692f13626211c07a2b4ce1f
El-iJah <38817251+El-iJah@users.noreply.github.com>
2018-05-03 15:17:28 +0300

    Initial commit

---
+++ apple.js
@@ -0,0 +1,9 @@
+let money = prompt('Сколько у Вас денег?');
+let apple = prompt('Сколько Вы купили яблок?');
+let bread = prompt('Сколько Вы купили батонов?');
+let hmapple = prompt('Сколько стоит одно яблоко?');
+let allapple = apple * hmapple;
+let hmbread = prompt ('Сколько стоит один батон?');
+let allbread = bread * hmbread;
+let output = money - (allapple + allbread);
+document.body.innerText=money>=output;
